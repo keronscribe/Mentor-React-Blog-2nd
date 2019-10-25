45a43d4d6842b32af7197f440245edec01758b33
Cain <Cain@chi.local>
2019-10-25 12:01:15 +0800

    Updates

---
+++ precache-manifest.8abf23efb20512a769798fb40084208b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef68c47b4786815f06b6625ae6739abb",
+    "url": "/Mentor-React-Blog-2nd/index.html"
+  },
+  {
+    "revision": "1791b29c84ebe6dd832a",
+    "url": "/Mentor-React-Blog-2nd/static/css/main.9361df8e.chunk.css"
+  },
+  {
+    "revision": "05d99ab8e39de5a17692",
+    "url": "/Mentor-React-Blog-2nd/static/js/2.7235c220.chunk.js"
+  },
+  {
+    "revision": "1791b29c84ebe6dd832a",
+    "url": "/Mentor-React-Blog-2nd/static/js/main.0b3b2f01.chunk.js"
+  },
+  {
+    "revision": "2a3cacc3fb6a62e73a93",
+    "url": "/Mentor-React-Blog-2nd/static/js/runtime-main.441b33b9.js"
+  }
+]);
