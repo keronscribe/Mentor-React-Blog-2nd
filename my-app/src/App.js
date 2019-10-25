a369bca2f60864b5b2e465dc1bed77b1efac586a
Cain <Cain@chi.local>
2019-10-25 14:29:40 +0800

    try again

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,10 +12,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Route path="/" exact component={Home} />
-        <Route path="/list" component={List} />
-        <Route path="/about" component={About} />
-        <Route path="/posts/:id" component={Post} />
+      <Route path="/"  exact component={Home} />
+      <Route path="/Mentor-React-sBlog-2nd/"  exact component={Home} />
+      <Route path="/list" component={List} />
+      <Route path="/about" component={About} />
+      <Route path="/posts/:id" component={Post} />
     </div>
   );
 }
